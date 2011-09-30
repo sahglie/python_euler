06f5af3c49f77d66b5ade8faaa9e266ab88d414b
sahglie <sahglie@gmail.com>
2011-09-29 17:41:20 -0700

    solved 22

---
+++ lib/022/s1.py
@@ -0,0 +1,29 @@
+#!/opt/local/bin/python
+
+import re
+
+"""Using names.txt, a 46K text file containing over five-thousand first
+names, begin by sorting it into alphabetical order. Then working out the
+alphabetical value for each name, multiply this value by its alphabetical
+position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which
+is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So,
+COLIN would obtain a score of 938  53 = 49714.
+
+What is the total of all the name scores in the file?
+"""
+
+EXPR = re.compile(r'([A-Z]+)')
+
+def clean(name):
+    return re.search(EXPR, name).group(1)
+
+def name_value(position, word):
+    return sum(ord(c) - 64 for c in word) * position
+
+if __name__ == "__main__":
+    with open("names.txt") as fd:
+        names = sorted(map(clean, fd.read().split(",")))
+        print sum(name_value(i+1, name) for i, name in enumerate(names))
+
