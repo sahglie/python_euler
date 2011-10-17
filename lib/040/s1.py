3f0228d6ec1cf010fcb866cba395baad5a3ad686
sahglie <sahglie@gmail.com>
2011-10-17 11:58:40 -0700

    use reduce

--- lib/040/s1.py
+++ lib/040/s1.py
@@ -15,6 +15,8 @@ value of the following expression.
 d1 * d10 * d100 * d1000 * d10000 * d100000 * d1000000
 """
 
+import operator
+
 def find_nth_digit(n):
     digits = 0
     for i in xrange(1, n+1):
@@ -28,7 +30,4 @@ def find_nth_digit(n):
 
 
 if __name__ == "__main__":
-    prod = 1
-    for i in xrange(0, 7):
-        prod *= find_nth_digit(10**i)
-    print prod
+    print reduce(operator.mul, [find_nth_digit(10**i) for i in xrange(0, 7)])
