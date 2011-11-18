66086c80ee44ba44c569ce8a9842228ef835fb1e
sahglie <sahglie@gmail.com>
2011-11-18 13:48:04 -0800

    more fun w/itertools

--- lib/038/s2.py
+++ lib/038/s2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import itertools
+from itertools import chain, product, groupby
 
 PANDIGITALS = set('123456789')
 def is_pandigital(n):
@@ -9,8 +9,12 @@ def is_pandigital(n):
 
 if __name__ == "__main__":
     pandigitals = []
-    product_pairs = itertools.product([9, 99, 999, 9999], [1, 2, 3, 4, 5])
-    for key, group in itertools.groupby(product_pairs, lambda x: x[0]):
-        product = int("".join([str(pair[0]*pair[1]) for pair in group]))
-        if is_pandigital(product): pandigitals.append(product)
+    nines = [[9], range(90, 100), range(900, 1000), range(9000, 10000)]
+    product_pairs = product(chain(*nines), [1, 2, 3, 4, 5])
+    for key, group in groupby(product_pairs, lambda x: x[0]):
+        group = [str(pair[0]*pair[1]) for pair in group]
+        while len(group) > 1:
+            pandigital = int("".join(group))
+            if is_pandigital(pandigital): pandigitals.append(pandigital)
+            group.pop()
     print max(pandigitals)
