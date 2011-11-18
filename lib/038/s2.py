91c19ace03d1ac2b012bf187b316a48500084fc0
sahglie <sahglie@gmail.com>
2011-11-18 12:02:55 -0800

    tersify

--- lib/038/s2.py
+++ lib/038/s2.py
@@ -5,8 +5,12 @@ cmd_folder = os.path.abspath(os.path.curdir).split("/")
 cmd_folder.pop()
 sys.path.insert(0, "/".join(cmd_folder))
 
-from utils import is_pandigital
 import itertools
+
+PANDIGITALS = set('123456789')
+def is_pandigital(n):
+    digits = str(n)
+    return len(digits) == 9 and set(digits) == PANDIGITALS
 
 if __name__ == "__main__":
     pandigitals = []
