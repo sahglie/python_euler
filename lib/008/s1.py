3c3d28d3e22e811bce5bd66a8e77cf602533db1b
sahglie <sahglie@gmail.com>
2012-06-26 22:02:17 -0700

    update 008 solution

--- lib/008/s1.py
+++ lib/008/s1.py
@@ -1,15 +1,20 @@
-#!/opt/local/bin/python
+#!/usr/bin/env python
+
+"""Find the greatest product of five consecutive digits in the 1000-digit number"""
+
+from functools import reduce
+import os
+
+file_path = os.path.join(os.path.dirname(__file__), "data.txt")
+f = open(file_path)
+data = "".join([l.strip() for l in f.readlines()])
+f.close()
 
 
-def build_number():
-    fd = open("data.txt")
-    parts = [s.strip() for s in fd.readlines()]
-    fd.close()
-    return "".join(parts)
+product = 0
+for (i,n) in enumerate(data):
+    if len(data[i:i+5]) == 5:
+        p = reduce(lambda x,y: int(x)*int(y), list(data[i:i+5]))
+        if p > product: product = p
 
-
-if "__main__" == __name__:
-    number_str = build_number()
-    prod_func = lambda x,y: x*y
-    print max(reduce(prod_func, (int(n) for n in number_str[i:i+5]))
-              for i in range(0, len(number_str)-5))
+print(product)
